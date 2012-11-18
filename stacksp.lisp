fefa76ebf01b70215b1bc9e34405a4a27daa2745
Derek VerLee <derekverlee@gmail.com>
2012-11-18 18:58:53 -0500

    Fix REPL buffer issue
    http://stackoverflow.com/questions/13445341/why-does-this-loop-print-the-value-from-the-previous-iteration

--- stacksp.lisp
+++ stacksp.lisp
@@ -1,5 +1,5 @@
 
-(defun process-stack (expression stack) 
+(defun process-stack (expression &optional stack) 
   (let ((item (car expression))
 	(rest (cdr expression)))
     (cond
@@ -8,3 +8,18 @@
       (process-stack rest (append stack (list item))))
      ((fboundp item)
       (process-stack rest (list (apply (symbol-function item) stack)))))))
+
+(defun read-stack-from-line ()
+  ( let 
+    ((line (read-line))) 
+    (with-input-from-string (in line) 
+			    (loop for
+				  x = (read in nil nil) 
+				  while x 
+				  collect x))))
+(defun stackp-repl ()
+  (loop for result = (process-stack (read-stack-from-line))
+        until (equal result '(done))
+        do
+        (format t "~a~%" result)
+        (finish-output)))
