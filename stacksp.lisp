40e65407c02ef5ae523a82552bfaab60ada03fe5
Derek VerLee <derekverlee@gmail.com>
2012-11-18 23:10:22 -0500

    Ability to extend the language with new functions in the root context

--- stacksp.lisp
+++ stacksp.lisp
@@ -1,12 +1,33 @@
 
-(defun make-context ()
-  (lambda (s)
-    (cond
-     ((equal s '+) (lambda (stack) (list (apply #'+ stack))))
-     (T (lambda (stack) (cons s stack))))))
+(defparameter root-context
+  (let* 
+      (
+       (ftbl (make-hash-table))
+       (lookup 
+	(lambda (s)
+	  (or (gethash s ftbl)
+	      (lambda (stack) (cons s stack)))))
+       (add
+	(lambda (s f)
+	       (setf (gethash s ftbl) f))))
+    (vector lookup add)))
+
+(defun make-context (&optional parent-context)
+  (let ((parent (or parent-context root-context))
+	(ftbl (make-hash-table)))
+    (let
+	((lookup (lambda (s) 
+		   (or (gethash s ftbl)
+		       (resolve parent s))))
+	 (add (lambda (s f) (setf (gethash s ftbl) f))))
+      (vector lookup add))))
 
 (defun resolve (context symb)
-  (funcall context symb))
+  (funcall (elt context 0) symb))
+
+(defun add-function (context symb function)
+  (funcall (elt context 1) symb function))
+
 
 (defun process-stack (expression &optional stack context)
   (if (null expression)
@@ -38,3 +59,7 @@
   (loop for result = (process-stack (read-stack-from-line))
         until (equal result '(done))
         do (print-stack-immediate result)))
+
+
+(add-function root-context '+ (lambda (stack) (list (apply #'+ stack))))
+(add-function root-context '- (lambda (stack) (list (apply #'- stack))))
