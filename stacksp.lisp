b47d467596938c4a8676f0dbac6f7796a816e508
Derek VerLee <derekverlee@gmail.com>
2012-11-18 22:33:36 -0500

    Things get functional, and we add the concept of a context

--- stacksp.lisp
+++ stacksp.lisp
@@ -1,12 +1,21 @@
 
-(defun process-stack (expression &optional stack)
+(defun make-context ()
+  (lambda (s)
+    (cond
+     ((equal s '+) (lambda (stack) (list (apply #'+ stack))))
+     (T (lambda (stack) (cons s stack))))))
+
+(defun resolve (context symb)
+  (funcall context symb))
+
+(defun process-stack (expression &optional stack context)
   (if (null expression)
       stack
-    (let* ((item (pop expression))
-	   (rest expression))
-      (if (and (symbolp item) (fboundp item))
-	  (process-stack rest (list (apply (symbol-function item) stack)))
-	(process-stack rest (push item stack))))))
+    (let* ((ctx (or context (make-context)))
+	   (item (pop expression))
+	   (rest expression)
+	   (fun (resolve ctx item)))
+      (process-stack rest (funcall fun stack)))))
 
 (defun string-to-stack (str)
   (with-input-from-string (in str)
@@ -22,8 +31,8 @@
   (string-to-stack (read-line)))
 
 (defun print-stack-immediate (stack)
-    (write-line (stack-to-string stack))
-    (finish-output))
+  (write-line (stack-to-string stack))
+  (finish-output))
 
 (defun stackp-repl ()
   (loop for result = (process-stack (read-stack-from-line))
