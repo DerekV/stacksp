e5a0b865f93a5a35e42e2c3f5bd5ff7118743d2c
Derek VerLee <derekverlee@gmail.com>
2012-11-18 20:17:08 -0500

    Improve processor to handle more types of input, code improvement

--- stacksp.lisp
+++ stacksp.lisp
@@ -1,25 +1,31 @@
 
-(defun process-stack (expression &optional stack) 
+(defun process-stack (expression &optional stack)
   (let ((item (car expression))
 	(rest (cdr expression)))
     (cond
      ((null expression) stack)
-     ((numberp item)
-      (process-stack rest (append stack (list item))))
-     ((fboundp item)
-      (process-stack rest (list (apply (symbol-function item) stack)))))))
+     ((and (symbolp item) (fboundp item))
+      (process-stack rest (list (apply (symbol-function item) stack))))
+     (T
+      (process-stack rest (append stack (list item)))))))
+
+(defun string-to-stack (str)
+  (with-input-from-string (in str)
+			  (loop for
+				x = (read in nil nil)
+				while x
+				collect x)))
+(defun stack-to-string (stack)
+  (format nil "~a" stack))
 
 (defun read-stack-from-line ()
-  ( let 
-    ((line (read-line))) 
-    (with-input-from-string (in line) 
-			    (loop for
-				  x = (read in nil nil) 
-				  while x 
-				  collect x))))
+  (string-to-stack (read-line)))
+
+(defun print-stack-immediate (stack)
+    (write-line (stack-to-string stack))
+    (finish-output))
+
 (defun stackp-repl ()
   (loop for result = (process-stack (read-stack-from-line))
         until (equal result '(done))
-        do
-        (format t "~a~%" result)
-        (finish-output)))
+        do (print-stack-immediate result)))
